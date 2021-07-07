eda24b4be7b30a10ba060064829fc8b5eba5add6
MayKama <62993571+MayKama@users.noreply.github.com>
2021-07-07 14:02:25 +0100

    Update calculatorgame.py

--- calculatorgame.py
+++ calculatorgame.py
@@ -1,7 +1,7 @@
-num1 = float(input("enter first number: "))
+num1 = float(input("enter first number1: "))
 
 op = input("enter opertor: ")
-num2 = float(input("enter second number: "))
+num2 = float(input("enter second number2: "))
 
 if op == "+" :
     print(num1 + num2)
@@ -15,5 +15,6 @@ elif op == "/":
     print(num1/ num2)
     
 else:
+    print()
     print("INAVLID OPERATOR: PLEASE ENTER A VALID OPERATOR")       
     
